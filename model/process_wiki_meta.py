f15103d01953b2f4387bd9df80dff14873dc30dc
imdeepmind <infinityatme@gmail.com>
2019-07-22 19:55:14 +0530

    added code for processing imdb wiki mat fues

--- model/process_wiki_meta.py
+++ model/process_wiki_meta.py
@@ -5,9 +5,9 @@ from scipy.io import loadmat
 from utils import compare_date, clean_full_date
 
 ## LOADING THE .MAT FILE
-imdb_mat = './dataset/unprocessed/imdb_crop/imdb.mat'
-imdb_data = loadmat(imdb_mat)
-imdb = imdb_data['imdb']
+wiki_mat = './dataset/unprocessed/wiki_crop/wiki.mat'
+wiki_data = loadmat(wiki_mat)
+wiki = wiki_data['wiki']
 
 ## PROCESS THE DATA
 
@@ -15,10 +15,10 @@ imdb = imdb_data['imdb']
 # the dob in the filename of each image also. In the image filename, it is stored as normal date. 
 # So reading that is simpler
 
-up_full_path = imdb[0][0][2][0] # Path of the images
-up_gender = imdb[0][0][3][0] # Gender
-up_face_score1 = imdb[0][0][6][0] # Score of the first image
-up_face_score2 = imdb[0][0][7][0] # Score of the second image (NaN if there is no second image)
+up_full_path = wiki[0][0][2][0] # Path of the images
+up_gender = wiki[0][0][3][0] # Gender
+up_face_score1 = wiki[0][0][6][0] # Score of the first image
+up_face_score2 = wiki[0][0][7][0] # Score of the second image (NaN if there is no second image)
 
 # Getting the gender
 p_gender = []
@@ -37,31 +37,31 @@ p_path = []
 
 for path in up_full_path:
     temp = path[0].split('_')
-    photo_taken = temp[3].split('.')[0]
-    dob = clean_full_date(temp[2])
+    photo_taken = temp[2].split('.')[0]
+    dob = clean_full_date(temp[1])
     p_dob.append(compare_date(dob, photo_taken))
-    p_path.append('imdb_crop/' + path[0])
+    p_path.append('wiki_crop/' + path[0])
 
 
 # Stacking the data
-imdb_processed_data = np.vstack((p_dob, p_gender, p_path, up_face_score1, up_face_score2)).T
+wiki_processed_data = np.vstack((p_dob, p_gender, p_path, up_face_score1, up_face_score2)).T
 
 ## SAVING THE DATA
 
 # Making a datagrame from the data
 cols = ['age', 'gender', 'path', 'f1_score', 'f2_score']
-imdb_df = pd.DataFrame(imdb_processed_data)
-imdb_df.columns = cols
+wiki_df = pd.DataFrame(wiki_processed_data)
+wiki_df.columns = cols
 
 # Filtering the data
-imdb_df = imdb_df[imdb_df['f1_score'] != '-inf']
-imdb_df = imdb_df[imdb_df['f2_score'] == 'nan']
+wiki_df = wiki_df[wiki_df['f1_score'] != '-inf']
+wiki_df = wiki_df[wiki_df['f2_score'] == 'nan']
 
 # Removing the f1 and f2 score
-imdb_df = imdb_df.drop(['f1_score', 'f2_score'], axis=1)
+wiki_df = wiki_df.drop(['f1_score', 'f2_score'], axis=1)
 
 # Shuffling the data
-imdb_df = imdb_df.sample(frac=1)
+wiki_df = wiki_df.sample(frac=1)
 
 # Storing as csv file
-imdb_df.to_csv('./dataset/processed/imdb_meta.csv', index=False)
+wiki_df.to_csv('./dataset/processed/wiki_meta.csv', index=False)
