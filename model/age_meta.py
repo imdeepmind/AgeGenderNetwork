a24d1cc11c04a4afcc0b388f52db03a4e62393a7
imdeepmind <infinityatme@gmail.com>
2019-08-01 20:07:56 +0530

    fixed filter issues in age_meta.py

--- model/age_meta.py
+++ model/age_meta.py
@@ -4,6 +4,7 @@ data = pd.read_csv('./dataset/processed/meta.csv', error_bad_lines=False)
 
 data = data.drop(['gender'], axis=1)
 
-data = data[data['age'] != -1]
+data = data[data['age'] > 0]
+data = data[data['age'] < 101]
 
 data.to_csv('./dataset/processed/age.csv', index=False)
